46a77641c2979cacc653c3013f9c01b6289d8c2a
Martin Barrios <martin100395@gmail.com>
2021-05-01 20:46:45 -0300

    Agregando el formulario

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MYSQL CRUD</title>
+    <!-- BOOSTRAP 4 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" 
+    integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+
+</nav>
--- index.php
+++ index.php
@@ -1,3 +1,43 @@
-<?php
-    echo "Hola mundo"
-?>
+<?php include("db.php")?>
+<?php include("includes/header.php")?>
+
+    <div class="container p-4">
+    
+        <div class="row">
+
+            <div class="col-md-4">
+
+                <div class="card card-body">
+
+                    <form action="">
+
+                        <div class="form-group">
+
+                            <input type="text" name="title" class="form-control"
+                             placeholder="Titulo de la tarea" autofocus>
+                         </div>
+
+                        <div class="form-group">
+                            <textarea name="description"  rows="2" class="form-control" 
+                            placeholder="Descripción de la tarea"></textarea>
+                        </div>
+
+                        <input type="submit" class="btn btn-success btn-block " name="save_task" value="Guardar tarea">
+
+                            
+                        
+                    </form>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+            
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <?php include("includes/footer.php")?>
+ 
